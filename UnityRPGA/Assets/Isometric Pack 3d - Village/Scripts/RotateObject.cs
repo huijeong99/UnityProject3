7b0ffdf8ee6da16a5dbc7451375c2a7df509dcf2
나희정 <hwayuya@gmail.com>
2020-06-19 17:32:00 +0900

    에셋 임포트

---
+++ UnityRPGA/Assets/Isometric Pack 3d - Village/Scripts/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour 
+{
+	[SerializeField]
+	private Vector3 Axis = Vector3.up;
+	[SerializeField]
+	private float Speed = 1f;
+
+	void Update () 
+	{
+		transform.Rotate(Axis * Time.deltaTime * Speed, Space.World);
+	}
+}
