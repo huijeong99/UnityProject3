4521c6553608d1d291a82c7e8a6529196d2d5b60
나희정 <hwayuya@gmail.com>
2020-06-25 17:36:13 +0900

    플레이어 이동 방향 카메라 고정 플레이어 컨트롤러 충돌범위 조정 플레이어 이동/애니메이션 스크립트 분리

--- UnityRPGA/Assets/Scripts/Player/PlayerCtrl.cs
+++ UnityRPGA/Assets/Scripts/Player/PlayerCtrl.cs
@@ -8,25 +8,23 @@ public class PlayerCtrl : MonoBehaviour
     public float speed = 5.0f;
     public Vector2 margin;  //뷰포트 좌표
     public VariableJoystick joystick;
-    public GameObject player;
 
     public float Rspeed = 150;
     float angleX;
 
-    enum State
-    {
-        Idle, Walk, Run, Attack1, Attack2, Hit, Die
-    }
+    //중력적용
+    public float gravity = -20;
+    float velocityY;    //낙하속도(벨로시티는 방향과 힘을 들고 있다)
+    float jumpPower = 10; //점프파워
+    int jumpCount = 0; //점프카운트
 
-    State state;
-    Animator Anim;
+    CharacterController cc;
 
     // Start is called before the first frame update
     void Start()
     {
-        state = State.Idle;
         margin = new Vector2(0, 0);
-        Anim=GetComponentInChildren<Animator>();
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -34,7 +32,6 @@ public class PlayerCtrl : MonoBehaviour
     {
         Move();
         Rotate();
-        Animation();
     }
 
 
@@ -48,11 +45,31 @@ public class PlayerCtrl : MonoBehaviour
             h = joystick.Horizontal;
             v = joystick.Vertical;
         }
-        
 
         Vector3 dir = new Vector3(h, 0, v);//3차원 이동
 
-        transform.position += dir * speed * Time.deltaTime;
+        dir = Camera.main.transform.TransformDirection(dir);
+        // transform.position += dir * speed * Time.deltaTime;
+
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            velocityY = 0;
+            jumpCount = 0;
+        }
+        else
+        {
+            //땅에 닿지 않은 상태이기때문에 중력적용하기
+            velocityY += gravity * Time.deltaTime;
+            dir.y = velocityY;
+        }
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            jumpCount++;
+            velocityY = jumpPower;
+        }
+
+        cc.Move(dir * speed * Time.deltaTime);
+
     }
 
 
@@ -67,34 +84,7 @@ public class PlayerCtrl : MonoBehaviour
 
         //회전각도를 직접 제어
         angleX += h * Rspeed * Time.deltaTime;
-        transform.eulerAngles = new Vector3(0, angleX, 0);
-    }
 
-    private void Animation()
-    {
-        switch (state)
-        {
-            case State.Idle:
-                Anim.SetTrigger("Idle");
-                break;
-            case State.Walk:
-                Anim.SetTrigger("Walk");
-                break;
-            case State.Run:
-                Anim.SetTrigger("Run");
-                break;
-            case State.Attack1:
-                Anim.SetTrigger("Attack1");
-                break;
-            case State.Attack2:
-                Anim.SetTrigger("Attack2");
-                break;
-            case State.Hit:
-                Anim.SetTrigger("Hit");
-                break;
-            case State.Die:
-                Anim.SetTrigger("Die");
-                break;
-        }
+        transform.eulerAngles = new Vector3(0, angleX, 0);
     }
 }
